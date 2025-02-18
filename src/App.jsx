62844340eee125049b921d53402d377251bd32eb
Jorge Paul <jserranopaul@gmail.com>
2025-02-18 15:41:41 +0100

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router';
 import { MainLayout } from '@/layouts';
 
-const Home = lazy(() => import('@/components/Home'));
-const Transitions = lazy(() => import('@/components/Transitions'));
+const Home = lazy(() => import('@/pages/Home'));
+const Forms = lazy(() => import('@/pages/Forms'));
+const Transitions = lazy(() => import('@/pages/Transitions'));
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path='forms' element={<Forms />} />
         <Route path='transitions' element={<Transitions />} />
       </Route>
     </Routes>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
-import Home from './Home';
 import ListArtists from './ListArtists';
 import ListPlaylists from './ListPlaylists';
 import ListSongs from './ListSongs';
-import Transitions from './Transitions';
 
-export { Home, ListArtists, ListPlaylists, ListSongs, Transitions };
+export { ListArtists, ListPlaylists, ListSongs };
