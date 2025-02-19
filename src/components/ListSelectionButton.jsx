6430e845db5fa80f0a58ee8a08e16ff1f6e253b9
Jorge Paul <jserranopaul@gmail.com>
2025-02-19 16:22:13 +0100

    useTransition

--- src/components/ListSelectionButton.jsx
+++ src/components/ListSelectionButton.jsx
@@ -1,12 +1,15 @@
-const ListSelectionButton = ({ disabled, label, setList }) => {
+import { useTransition } from 'react';
+
+const ListSelectionButton = ({ disabled, label, action }) => {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <button
       className='btn btn-success capitalize'
-      onClick={() => {
-        setList(label);
-      }}
-      disabled={disabled}
+      onClick={() => startTransition(action)}
+      disabled={disabled || isPending}
     >
+      {isPending && <span className='loading loading-spinner'></span>}
       {label}
     </button>
   );
--- src/pages/Transitions.jsx
+++ src/pages/Transitions.jsx
@@ -7,9 +7,21 @@ const Transitions = () => {
   return (
     <>
       <div className='flex gap-2'>
-        <ListSelectionButton setList={setList} label='playlists' disabled={list === 'playlists'} />
-        <ListSelectionButton setList={setList} label='artists' disabled={list === 'artists'} />
-        <ListSelectionButton setList={setList} label='songs' disabled={list === 'songs'} />
+        <ListSelectionButton
+          action={() => setList('playlists')}
+          label='playlists'
+          disabled={list === 'playlists'}
+        />
+        <ListSelectionButton
+          action={() => setList('artists')}
+          label='artists'
+          disabled={list === 'artists'}
+        />
+        <ListSelectionButton
+          action={() => setList('songs')}
+          label='songs'
+          disabled={list === 'songs'}
+        />
       </div>
       <div className='mt-4'>
         {list === 'playlists' && <ListPlaylists />}
